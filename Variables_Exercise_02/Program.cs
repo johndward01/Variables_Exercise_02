031f6ea07474b171bf18a27c6dc752c12bf1be8b
johndward01 <johndward01@gmail.com>
2023-02-22 03:09:26 -0600

    Add project files.

---
+++ Variables_Exercise_02/Program.cs
@@ -0,0 +1,57 @@
+﻿
+// TODO: Declare and initialize a variable of type bool called myBool 
+
+
+// TODO: Declare and initialize a variable of type byte called myByte
+
+
+// TODO: Declare and initialize a variable of type sbyte called mySByte 
+
+
+// TODO: Declare and initialize a variable of type char called myChar 
+
+
+// TODO: Declare and initialize a variable of type decimal called myDecimal 
+
+
+// TODO: Declare and initialize a variable of type double called myDouble 
+
+
+// TODO: Declare and initialize a variable of type float called myFloat 
+
+
+// TODO: Declare and initialize a variable of type int called myInt 
+
+
+// TODO: Declare and initialize a variable of type uint called myUInt 
+
+
+// TODO: Declare and initialize a variable of type nint called myNInt 
+
+
+// TODO: Declare and initialize a variable of type long called myLong
+
+
+// TODO: Declare and initialize a variable of type ulong called myULong
+
+
+// TODO: Declare and initialize a variable of type short called myShort
+
+
+// TODO: Declare and initialize a variable of type ushort called myUShort
+
+
+// TODO: Declare and initialize a variable of type object called myObject
+
+
+// TODO: Declare and initialize a variable of type string called myString
+
+
+// TODO: Declare and initialize a variable of type dynamic called myDynamic 
+
+
+
+
+
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
