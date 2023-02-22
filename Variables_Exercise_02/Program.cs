aaf80f3cc6c4e0973ebed3ca2aac570b8c9c7ae5
johndward01 <johndward01@gmail.com>
2023-02-22 03:16:52 -0600

    done

--- Variables_Exercise_02/Program.cs
+++ Variables_Exercise_02/Program.cs
@@ -1,57 +1,53 @@
 ﻿
 // TODO: Declare and initialize a variable of type bool called myBool 
-
+bool myBool = true; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/types#835-simple-types
 
 // TODO: Declare and initialize a variable of type byte called myByte
-
+byte myByte = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type sbyte called mySByte 
-
+sbyte mySByte = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type char called myChar 
-
+char myChar = '\0'; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type decimal called myDecimal 
-
+decimal myDecimal = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
 
 // TODO: Declare and initialize a variable of type double called myDouble 
-
+double myDouble = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
 
 // TODO: Declare and initialize a variable of type float called myFloat 
-
+float myFloat = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
 
 // TODO: Declare and initialize a variable of type int called myInt 
-
+int myInt = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type uint called myUInt 
-
+uint myUInt = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type nint called myNInt 
-
+nint myNInt = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type long called myLong
-
+long myLong = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type ulong called myULong
-
+ulong myULong = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type short called myShort
-
+short myShort = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type ushort called myUShort
-
+ushort myUShort = 0; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 // TODO: Declare and initialize a variable of type object called myObject
-
+object myObj = null; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/types#823-the-object-type
 
 // TODO: Declare and initialize a variable of type string called myString
-
+string myStr = null; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/types#825-the-string-type
 
 // TODO: Declare and initialize a variable of type dynamic called myDynamic 
+dynamic myDynamic = null; // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 
 
-
-
-
-
-// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
